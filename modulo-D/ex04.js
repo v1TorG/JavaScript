b11b03fb23cc0af440b4fc6362249154f82b5912
viTor <119415099+v1TorG@users.noreply.github.com>
2025-02-01 01:10:41 -0300

    nova att

---
+++ modulo-D/ex04.js
@@ -0,0 +1,14 @@
+var idade = 18
+
+console.log(`Você tem ${idade}`)
+
+if (idade < 16){
+    console.log('não vota')
+
+} else if ( idade < 18 || idade > 65) {
+    console.log('voto opcional')
+
+} else {
+        console.log('voto obrigatorio')
+}
+
