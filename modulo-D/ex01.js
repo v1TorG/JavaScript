1ce47ddd23f3280fe1f1cb81d749d63fd6c0f690
viTor <119415099+v1TorG@users.noreply.github.com>
2025-02-01 00:20:56 -0300

    att

---
+++ modulo-D/ex01.js
@@ -0,0 +1,6 @@
+var vel = 75.5
+console.log('velocidade do seu carro ' + vel + 'km/h')
+if (vel > 60) { //condição simples
+    console.log('MULTADO')
+}
+console.log('dirija com segurança :D')
---
+++ modulo-D/ex02.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log('vivendo em '+ pais)
+if (pais == 'brasil'){
+    console.log('brasileiro')
+} else { //condição composta
+    console.log('estrangeiro')
+}
