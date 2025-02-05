f5fe5ce823385719afbfa9574c26c24fc3136685
viTor <119415099+v1TorG@users.noreply.github.com>
2025-02-05 01:26:51 -0300

    finalizando atividade 2

--- modulo-D/atividade02/script.js
+++ modulo-D/atividade02/script.js
@@ -7,7 +7,44 @@ function verificar() {
     if (fano.value.length == 0 || fano.value > ano){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        window.alert('ok')
+        var fsex = document.getElementsByName('sex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 4){
+               img.setAttribute('src', 'bbM.jpg') 
+            } else if (idade < 11){
+                img.setAttribute('src', 'garotoM.jpg') 
+            } else if (idade < 24){
+                img.setAttribute('src', 'jovemM.jpg') 
+            } else if (idade < 55){
+                img.setAttribute('src', 'homemM.jpg') 
+            } else {
+                img.setAttribute('src', 'velhoM.jpeg') 
+            }
+
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 4){
+                img.setAttribute('src', 'bbF.jpg') 
+            } else if (idade < 11){
+                img.setAttribute('src', 'garotaF.jpg') 
+            } else if (idade < 24){
+                img.setAttribute('src', 'jovemF.jpg') 
+            } else if (idade < 55){
+                img.setAttribute('src', 'mulherF.jpg') 
+            } else {
+                img.setAttribute('src', 'idodaF.jpg') 
+            }
+
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Dedectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }                                          
     
