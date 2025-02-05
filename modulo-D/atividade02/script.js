079581d19e438ff4f4dc1154163bfe432259e2de
viTor <119415099+v1TorG@users.noreply.github.com>
2025-02-05 00:04:54 -0300

    att nova atividade

---
+++ modulo-D/atividade02/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = (`Agora são ${hora} horas.`)
+
+    if (hora >= 0 && hora < 12){
+        document.body.style.background = '#f0e15b'
+        img.src = 'manhã.jpg'
+    } else if (hora >= 12 && hora <18){
+        document.body.style.background = '#e9895d'
+        img.src = 'tarde.jpg'
+    } else {
+        document.body.style.background = '#5c5b5b'
+        img.scr = 'noite.jpg'
+    }
+}                                           
+    
