cbba5361382dfe5fa9e6e7c00c766762dff8da67
viTor <119415099+v1TorG@users.noreply.github.com>
2025-02-06 00:58:11 -0300

    att while

---
+++ modulo-E/ex01.js
@@ -0,0 +1,15 @@
+var c = 1 
+while (c <= 6){
+    console.log('tudo bem')
+    c++ // c = c + 1
+}
+
+console.log('-----------------') //apenas para separar a op1 da op2
+// OPÇÃO 2 - UTILIZANDO O 'do'
+ 
+var c = 1 
+do{
+    console.log('tudo bem 2')
+    c++ // c = c + 1
+}while (c <= 6)
+
