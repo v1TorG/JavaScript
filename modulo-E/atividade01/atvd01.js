f68ebea70b161b15ee7da84c6bd6c8c21cbc2f68
viTor <119415099+v1TorG@users.noreply.github.com>
2025-02-08 01:23:14 -0300

    finalizando atvd 1 E

--- modulo-E/atividade01/atvd01.js
+++ modulo-E/atividade01/atvd01.js
@@ -5,15 +5,30 @@ function contar(){
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
         window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} \u{1F449}`
+        if (p <= 0){
+            window.alert('Passo Invalido! Considerando passo 1')
+            p = 1
         }
+        if ( i < f){
+            //CONTAGEM CRESCENTE
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            
+        } else {
+            //CONTAGEM REGRESSIVA
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+            
+        }
+        res.innerText += `\u{1F3C1}`
     }
 }
